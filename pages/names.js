6552c0c6ce53b10295ebd4138c07788a754ae5eb
Larry Salibra <larry@appartisan.com>
2021-12-15 12:13:12 +0800

    app container

--- pages/names.js
+++ pages/names.js
@@ -1,10 +1,26 @@
 import React from "react";
 import Head from "next/head";
 import SignIn from "../components/SignIn";
+import App from "../components/App";
 
 import { userSession } from "../utils/auth";
 
+/**
+  Used to disable server-side rendering on this page
+*/
+function SafeHydrate({ children }) {
+  return (
+    <div suppressHydrationWarning>
+      {typeof window === "undefined" ? null : children}
+    </div>
+  );
+}
+
 class Names extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleSignOut = this.handleSignOut.bind(this);
+  }
   state = {
     userData: null,
   };
@@ -17,26 +33,26 @@ class Names extends React.Component {
 
   render() {
     return (
-      <div>
-        <Head>
-          <title>Renew your BNS names</title>
-        </Head>
+      <SafeHydrate>
+        <div>
+          <Head>
+            <title>Renew your BNS names</title>
+          </Head>
 
-        <main>
-          {!userSession.isUserSignedIn() ? (
-            <SignIn />
-          ) : (
-            <button
-              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-              onClick={(e) => this.handleSignOut(e)}
-            >
-              Sign out
-            </button>
-          )}
-        </main>
+          <main>
+            {!userSession.isUserSignedIn() ? (
+              <SignIn />
+            ) : (
+              <App
+                userData={this.state.userData}
+                signOut={this.handleSignOut}
+              />
+            )}
+          </main>
 
-        <footer></footer>
-      </div>
+          <footer></footer>
+        </div>
+      </SafeHydrate>
     );
   }
 }
