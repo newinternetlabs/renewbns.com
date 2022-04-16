d60bea945269327e4b68bc3457c92036c97b4b01
Larry Salibra <larry@appartisan.com>
2022-04-16 18:31:24 +0800

    try to compile to webbundle

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
+
+const HtmlInlineScriptPlugin = require("html-inline-script-webpack-plugin");
+
 module.exports = {
   reactStrictMode: true,
-}
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // Important: return the modified config
+    config.plugins.push(
+      new webpack.optimize.LimitChunkCountPlugin({
+        maxChunks: 1, // disable creating additional chunks
+      })
+    );
+
+    config.plugins.push(new HtmlInlineScriptPlugin());
+    return config;
+  },
+};
