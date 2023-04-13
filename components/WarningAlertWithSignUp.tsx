a9fab12c988ea85f496a79528e11cf0ceac67a6d
Larry Salibra <larry@newinternetlabs.com>
2023-04-13 17:46:33 +0800

    change warning copy to prompt upgrade

--- components/WarningAlertWithSignUp.tsx
+++ components/WarningAlertWithSignUp.tsx
@@ -9,17 +9,8 @@ const WarningAlertWithSignUp = (props: { setShowNotifyModal: Function }) => {
         </div>
         <div className="ml-3 flex-1 md:flex md:justify-between">
           <p className="text-sm text-red-700">
-            This is a pre-Stacks 2.0 legacy name.{" "}
-            <a
-              className="font-medium text-red-700 hover:text-red-600 cursor-pointer underline"
-              onClick={(e) => {
-                e.preventDefault();
-                props.setShowNotifyModal(true);
-              }}
-            >
-              Sign up
-            </a>{" "}
-            to be notified when you can upgrade it for use with Stacks 2.0 apps.
+            This is a pre-Stacks 2.0 legacy name. Please upgrade it so that you
+            can continue to use it.
           </p>
         </div>
       </div>
